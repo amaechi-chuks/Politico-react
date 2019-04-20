269484fb32d582d73456792c1c1785e691e2e784
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-04-20 13:51:42 +0100

    chore(setup): implement project setup
    - install dependencies
    - intall devdependencies
    - setup test using jest and enzyme
    - setup prettier
    - setup eslint airbnb

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+// import Header from './Header/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>hello</h1>
+      </div>
+    )
+  }
+}
+
