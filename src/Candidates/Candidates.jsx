98b662f77f8081cdfab968ffd6087bfe26d5c81d
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-05-04 16:40:07 +0100

    165656176-feature(candidate views): implement a view page for all candidates - create a candaidate componenet
    [Finishes #165656176]

---
+++ src/Candidates/Candidates.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CandidateList from './CandidateList';
+import Header from '../Components/Header/Header';
+
+const Candidates = () => {
+  return (
+    <div>
+      <Header />
+      <CandidateList />
+    </div>
+  );
+};
+export default Candidates;
